f14f98df1a082b4217b931df4e2df19b6193b786
gessicameireles30 <gessicadasilvameireles@gmail.com>
2025-07-19 19:25:09 -0300

    feat: create TabelafipeApplication

---
+++ src/main/java/com/api/tabelafip/TabelaFip/TabelafipeApplication.java
@@ -0,0 +1,21 @@
+package com.api.tabelafip.TabelaFip;
+
+import com.api.tabelafip.TabelaFip.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TabelafipeApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelafipeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+	}
+}
