db6d23764d2b2016b91222f3fc8afd3d501ab425
gessicameireles30 <gessicadasilvameireles@gmail.com>
2025-07-19 19:20:37 -0300

    feat: create model Dados

---
+++ src/main/java/com/api/tabelafip/TabelaFip/model/Dados.java
@@ -0,0 +1,4 @@
+package com.api.tabelafip.TabelaFip.model;
+
+public record Dados(String codigo, String nome) {
+}
---
+++ src/test/java/com/api/tabelafip/TabelaFip/TabelaFipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.tabelafip.TabelaFip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
